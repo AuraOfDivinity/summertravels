f82af95031c1d0a5e5e74dc054507ebc18cb2647
Asel <peirisasel373@gmail.com>
2020-08-30 16:50:48 +0530

    Fixing syntax errors

--- src/data/data.js
+++ src/data/data.js
@@ -5,7 +5,7 @@ const locationList = [
   {
     title: "Caspersen Beach",
     currentHotspot: "beach",
-    video: https://www.youtube.com/watch?v=tBFdp7-TAHg,
+    video: "https://www.youtube.com/watch?v=tBFdp7-TAHg",
     facts: [
       "Good place to find prehistoric sharks' teeth!",
       "Has freshwater and saltwater marshes, mangroves and tidal flats",
@@ -17,7 +17,7 @@ const locationList = [
   {
     title: "Hamoa Beach",
     currentHotspot: "beach2",
-    video: https://youtu.be/m_QgCPBE1kY?t=9,
+    video: "https://youtu.be/m_QgCPBE1kY?t=9",
     facts: [
       "Popular surf break",
       "Once known as 'Mokae' because of the cliff around it",
@@ -26,10 +26,10 @@ const locationList = [
     location: [20, -155],
     nextHotspot: "beach3",
   },
-   {
+  {
     title: "Ventura Beach",
     currentHotspot: "beach3",
-    video: https://www.youtube.com/watch?v=9mlgwus_zew,
+    video: "https://www.youtube.com/watch?v=9mlgwus_zew",
     facts: [
       "Ventura Beach is a group of beaches",
       "Has a passage all the way to McGrath State Beach",
@@ -38,10 +38,10 @@ const locationList = [
     location: [40, 79],
     nextHotspot: "beach4",
   },
-   {
+  {
     title: "Cala Millor Beach",
     currentHotspot: "beach4",
-    video: https://youtu.be/4m11cfhb7ZM?t=23,
+    video: "https://youtu.be/4m11cfhb7ZM?t=23",
     facts: [
       "It's over a kilometre long",
       "The depth is about 30 metres",
